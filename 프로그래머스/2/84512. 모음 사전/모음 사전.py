c6de4ba55091c68afe0be18bad7528e03904b204
hellokms8828 <66521935+hellokms8828@users.noreply.github.com>
2024-05-30 10:11:02 +0900

    [level 2] Title: 모음 사전, Time: 1.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,10 @@
 
 from itertools import product
 def solution(word):
-    answer = 0
     myList = []
     for i in range(1,6):
         myList.extend(list(map(''.join, product(['A','E','I','O','U'], repeat=i))))
+    print(myList)
     myList.sort()
     
     return myList.index(word)+1
