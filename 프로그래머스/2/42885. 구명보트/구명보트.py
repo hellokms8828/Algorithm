b7ac13b3ff32ff9fb791e129fbf6ce7cc939fc56
hellokms8828 <66521935+hellokms8828@users.noreply.github.com>
2024-06-01 00:20:13 +0900

    [level 2] Title: 구명보트, Time: 7.93 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,12 @@
+def solution(people, limit):
+    people.sort()
+    left, right = 0, len(people) - 1
+    boats = 0
+    
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
+        boats += 1
+    
+    return boats
